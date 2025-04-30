a65d3de360ba3bd925858daf7c01260f6d08fda1
Anurag1096 <anuragqwerty09@outlook.com>
2025-04-30 22:37:31 +0530

    fix:message was printing twice

--- guess_a_num.asm
+++ guess_a_num.asm
@@ -1,14 +1,22 @@
 section .data                 ;Initilized data
 ;ask to enter a number
 msg1     db     "Enter a number less than 100 to guess"
+lenmsg1 equ     $-msg1
+
 num      dd     35
+
 game_won db     "Congratulation you have guessed corectly"
-lesser   db     "The Guessed value is less than the number"
-len_lt   equ    $-lesser
-greater  db     "The Guessed value is greater than the number"
-len_gt   equ    $-greater
 len_gw   equ    $-game_won
-lenmsg1  equ    $-msg1
+lesser   db     "The Guessed value is less than the number"
+
+len_lt   equ    $-lesser
+
+greater  db     "The Guessed value is greater than the number"
+
+len_gt   equ    $-greater
+
+
+
 newline  db     0x0A         ;new line 1 byte
 
 ;------------------
@@ -23,7 +31,7 @@ section .text                 ;Code section
 
 
 _start:
- ;Printing the msg
+;Printing the msg
  mov eax, 4                   ;Syscall number for write
  mov ebx,1                    ;file discripter for stdout
  mov ecx, msg1                ;The msg to display
@@ -31,27 +39,45 @@ _start:
  int 80h                      ;interupt for sys call execution
  jmp l1
  ;call excepts a return keyword
+ xor ecx,ecx
+ xor edx,edx
 l1:
  call new_line
  call read_data
 
-;looping structure
-;there will be first check if the number is same
-
  call parse_input
-                ; target number
  cmp ebx ,[num]
- ;Check if equal
  je  equal
  
  jl less_than
- ;check if greater than
- jg  greater_than
- ;if not greater or equal then less than the num
  
+ jg  greater_than
+
 ;--------------------
+
+;--------------------
+read_data:
+   mov eax,3
+   mov ebx, 0
+   mov ecx,user_input
+   mov edx,4
+   int 80h
+   ret
+;--------------------
+new_line:
+;change line
+   mov eax,4
+   mov ebx,1
+   mov ecx ,newline
+   mov edx,1
+   int 80h
+   ret
+ ;exit
+
 less_than:
  ;if less than
+ xor ecx,ecx
+ xor edx,edx
  mov eax,4
  mov ebx,1
  mov ecx,lesser
@@ -62,6 +88,8 @@ less_than:
  jmp l1
 ;--------------------
 greater_than:
+ xor ecx,ecx
+ xor edx,edx
  mov eax,4
  mov ebx,1
  mov ecx,greater
@@ -81,24 +109,7 @@ equal:
   
   call new_line
   jmp end_program
-;--------------------
-read_data:
-   mov eax,3
-   mov ebx, 0
-   mov ecx,user_input
-   mov edx,4
-   int 80h
-   ret
-;--------------------
-new_line:
-;change line
-   mov eax,4
-   mov ebx,1
-   mov ecx ,newline
-   mov edx,1
-   int 80h
-   ret
- ;exit
+
 ;--------------------
 
 end_program: 
